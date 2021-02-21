03cd04b54aa8eca24d946707c0d7151dd157372a
dm1tro69 <dimolg22@gmail.com>
2021-02-21 21:10:10 +0200

    add react-router-dom , End section5

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/style.css'
 import App from './app/layout/App.jsx';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+
+    , document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
